f7a030ec03f9641ebed625161571e348146e318a
timetam <timetam24@naver.com>
2025-04-22 15:15:29 +0900

    [백준] 시험 성적 - 9498

---
+++ baekjoon/index_9498.js
@@ -0,0 +1,23 @@
+let fs = require(`fs`);
+const filePath =
+  process.platform === `linux` ? `/dev/stdin` : `./baekjoon/input.txt`;
+let input = fs.readFileSync(filePath).toString().trim();
+// let input = fs.readFileSync(filePath).toString().split(` `)
+//let input = fs.readFileSync(filePath).toString().split(`\n`)
+//let input = fs.readFileSync(filePath).toString().split(` `).map(function(a) {return +a})
+
+input = Number(input);
+let score = "";
+
+if (input >= 90 && input <= 100) {
+  score = "A";
+} else if (input >= 80 && input <= 89) {
+  score = "B";
+} else if (input >= 70 && input <= 79) {
+  score = "C";
+} else if (input >= 60 && input <= 69) {
+  score = "D";
+} else if (input <= 59 && input >= 0) {
+  score = "F";
+}
+console.log(score);
