c9e9e6f9d97bd6f001e52badff44d3b69bb3a38f
timetam <timetam24@naver.com>
2025-04-23 11:25:11 +0900

    [백준] 윤년 - 2753

---
+++ baekjoon/index_2753.js
@@ -0,0 +1,13 @@
+let fs = require(`fs`);
+const filePath =
+  process.platform === `linux` ? `/dev/stdin` : `./baekjoon/input.txt`;
+let input = fs.readFileSync(filePath).toString().trim();
+// let input = fs.readFileSync(filePath).toString().split(` `)
+//let input = fs.readFileSync(filePath).toString().split(`\n`)
+//let input = fs.readFileSync(filePath).toString().split(` `).map(function(a) {return +a})
+
+const isLeapYear = (year) => {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+};
+
+console.log(isLeapYear(+input) ? 1 : 0);
