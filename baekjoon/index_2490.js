240d77dcd6f1e3a1949ea189f9a5e9090a0a5741
timetam <timetam24@naver.com>
2025-04-23 12:19:12 +0900

    [백준] 윷놀이 - 2490

---
+++ baekjoon/index_2490.js
@@ -0,0 +1,40 @@
+let fs = require(`fs`);
+const filePath =
+  process.platform === `linux` ? `/dev/stdin` : `./baekjoon/input.txt`;
+// let input = fs.readFileSync(filePath).toString().trim()
+// let input = fs.readFileSync(filePath).toString().split(` `)
+let [firstArray, secondArray, thirdArray] = fs
+  .readFileSync(filePath)
+  .toString()
+  .split(`\n`)
+  .map((arr) => arr.split(` `).map(Number));
+//let input = fs.readFileSync(filePath).toString().split(` `).map(function(a) {return +a})
+
+const getResult = (array) => {
+  let countZero = 0;
+  let countOne = 0;
+
+  for (let num of array) {
+    if (num === 0) {
+      countZero++;
+    } else {
+      countOne++;
+    }
+  }
+
+  if (countZero === 0 && countOne === 3) {
+    return "A";
+  } else if (countZero === 2 && countOne === 2) {
+    return "B";
+  } else if (countZero === 3 && countOne === 1) {
+    return "C";
+  } else if (countZero === 4) {
+    return "D";
+  } else {
+    return "E";
+  }
+};
+
+console.log(getResult(firstArray));
+console.log(getResult(secondArray));
+console.log(getResult(thirdArray));
